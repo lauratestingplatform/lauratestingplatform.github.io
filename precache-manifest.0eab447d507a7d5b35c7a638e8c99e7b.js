9f593368c6a6ed6204ddb3b83014c90be2f3b156
lauratestingplatform <60147314+lauratestingplatform@users.noreply.github.com>
2020-01-21 16:04:30 +0000

    Add files via upload

---
+++ precache-manifest.0eab447d507a7d5b35c7a638e8c99e7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c75c6e5753eda1759b49d9ae6be80688",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6da83fd2f34a3fbc457d",
+    "url": "/static/css/main.876edbfc.chunk.css"
+  },
+  {
+    "revision": "5189d5fde21c9c45ed49",
+    "url": "/static/js/2.870c4045.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.870c4045.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6da83fd2f34a3fbc457d",
+    "url": "/static/js/main.44773f53.chunk.js"
+  },
+  {
+    "revision": "23ab70846ff2ed365202",
+    "url": "/static/js/runtime-main.19e5cc06.js"
+  },
+  {
+    "revision": "a55da94d66dbcacbe55a87970a438c65",
+    "url": "/static/media/creativepassportlogo.a55da94d.png"
+  }
+]);
